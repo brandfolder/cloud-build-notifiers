49831e296f889d39a4b412375700fdd027cd9098
Brett Nekolny <brett.nekolny@brandfolder.com>
2021-09-14 08:00:07 -0600

    gofmt.

--- slack/main_test.go
+++ slack/main_test.go
@@ -3,19 +3,19 @@ package main
 import (
 	"testing"
 
-    "github.com/google/go-cmp/cmp"
-    "github.com/slack-go/slack"
-    cbpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
+	"github.com/google/go-cmp/cmp"
+	"github.com/slack-go/slack"
+	cbpb "google.golang.org/genproto/googleapis/devtools/cloudbuild/v1"
 )
 
 func TestGetStoragePath(t *testing.T) {
-    n := new(slackNotifier)
-    result := n.getStoragePath("build-id")
-    expected := "messages/build-id"
-    if result != expected {
-        t.Errorf("Unexpected storage path: %q, expected: %q", result, expected)
-    }
-    return
+	n := new(slackNotifier)
+	result := n.getStoragePath("build-id")
+	expected := "messages/build-id"
+	if result != expected {
+		t.Errorf("Unexpected storage path: %q, expected: %q", result, expected)
+	}
+	return
 }
 
 func TestBuildAttachmentMessageOption(t *testing.T) {
@@ -38,19 +38,19 @@ func TestBuildAttachmentMessageOption(t *testing.T) {
 	got := n.buildAttachmentMessageOption(b)
 
 	want := slack.MsgOptionAttachments(
-        slack.Attachment{
-            Text:  "SUCCESS: :UNKNOWN_REPO: UNKNOWN_REPO (my-project-id) \u003chttps://some.example.com/log/url?foo=bar\u0026utm_campaign=google-cloud-build-notifiers\u0026utm_medium=chat\u0026utm_source=google-cloud-build|View Build\u003e\n*Branch*: UNKNOWN_BRANCH *Author*: UNKNOWN_COMMIT_AUTHOR \n\u003cUNKNOWN_COMMIT_URL|Commit\u003e *UNKNOWN_COMMIT_SHA*: UNKNOWN_COMMIT_MESSAGE",
+		slack.Attachment{
+			Text:  "SUCCESS: :UNKNOWN_REPO: UNKNOWN_REPO (my-project-id) \u003chttps://some.example.com/log/url?foo=bar\u0026utm_campaign=google-cloud-build-notifiers\u0026utm_medium=chat\u0026utm_source=google-cloud-build|View Build\u003e\n*Branch*: UNKNOWN_BRANCH *Author*: UNKNOWN_COMMIT_AUTHOR \n\u003cUNKNOWN_COMMIT_URL|Commit\u003e *UNKNOWN_COMMIT_SHA*: UNKNOWN_COMMIT_MESSAGE",
 			Color: "good",
 		},
 	)
 
-    _, gotValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", *got)
-    if err != nil {
-        t.Errorf("Unable to build message: %w", err)
-    }
-    _, wantValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", want)
+	_, gotValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", *got)
+	if err != nil {
+		t.Errorf("Unable to build message: %w", err)
+	}
+	_, wantValues, err := slack.UnsafeApplyMsgOptions("fake-token", "fake-channel", "https://fake.com/", want)
 	if diff := cmp.Diff(gotValues, wantValues); diff != "" {
-        t.Logf("full message: %v", gotValues)
+		t.Logf("full message: %v", gotValues)
 		t.Errorf("writeMessage got unexpected diff: %s", diff)
 	}
 	return
