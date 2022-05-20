3fcf8afd33b9386954179473262d53bf48916e29
Freddy Fredericks <brfredericks@gmail.com>
2022-05-20 12:22:01 -0700

    Added comment about race condition on delete with multi-region

--- slack/main.go
+++ slack/main.go
@@ -164,6 +164,7 @@ func (s *slackNotifier) SendNotification(ctx context.Context, build *cbpb.Build)
 	}
 
 	// Determine if we're done and need to delete the build from google cloud store
+	// NOTE: this may never work because of a race condition on multi-region, but dammit, we'll try!!
 	if shouldDeleteBuildFile(sb) {
 		log.Infof("Deleting the build file: %s", commitSha)
 		err = sc.Bucket(s.storageBucket).Object(getStoragePath(commitSha)).Delete(context.Background())
