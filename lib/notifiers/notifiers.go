dc539071ad8d50bd4f2bc0f29bfd0d76d6f9b3f3
Hannah BF <hannah.friedlaender@smartsheet.com>
2024-12-04 17:30:44 -0500

    Bump go version and address vulnerabilities (#177)

--- lib/notifiers/notifiers.go
+++ lib/notifiers/notifiers.go
@@ -291,7 +291,14 @@ func Main(notifier Notifier) error {
 	}
 
 	// Block on the HTTP's health.
-	return http.ListenAndServe(":"+port, nil)
+	srv := &http.Server{
+		Addr:              ":" + port,
+		Handler:           nil,
+		ReadTimeout:       60 * time.Second,
+		ReadHeaderTimeout: 60 * time.Second,
+	}
+
+	return srv.ListenAndServe()
 }
 
 func parseTemplate(ctx context.Context, tmpl *Template, grf gcsReaderFactory) (string, error) {
